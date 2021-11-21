4428dbd2e1d3864f67887a72aea76be51eb2e65b
Pirxcy <71592833+PirxcyFinal@users.noreply.github.com>
2021-11-21 10:40:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ async def on_ready():
 async def backend(ctx):
   try:
     status = await web.get(url='https://StoreAccounts.pirxcy1942.repl.co')
-    if status == {"status":"online"}:
+    if status == '{"status":"online"}':
       embed=discord.Embed(
         title="Backend Status.", 
         description=f"```json\n{status}\n```"
