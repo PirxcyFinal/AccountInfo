43a008bb809f0c4401cba026799bebe596ddd789
Pirxcy <71592833+PirxcyFinal@users.noreply.github.com>
2021-11-21 13:11:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ class backend:
 async def check_backend():
   status = await web.get(url='https://StoreAccounts.pirxcy1942.repl.co')
   print(status)
-  if status == {'status': 'Online'}:
+  if status == {"status":"Online"}:
     return status
   else:
     return False
